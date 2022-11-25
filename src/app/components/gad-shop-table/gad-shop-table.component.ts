a5e4b37c025cbd4adc5ca4017806798f89cdaaa4
Ramiro Meza <mezaramiroe@gmail.com>
2022-11-25 18:17:27 -0300

    url

--- src/app/components/gad-shop-table/gad-shop-table.component.ts
+++ src/app/components/gad-shop-table/gad-shop-table.component.ts
@@ -11,21 +11,21 @@ export class GadShopTableComponent implements OnInit {
   perros: perro[] = [
     {
       "id" : 1,
-      "imagen" : "./assets/img/dog4.jpg",
+      "imagen" : "../assets/img/dog4.jpg",
       "nombre" : "Huesos",
       "edad" : 2,
       "aptoDepto" : true
     },
     {
       "id" : 2,
-      "imagen" : "./assets/img/dog.jpg",
+      "imagen" : "../assets/img/dog.jpg",
       "nombre" : "Hice",
       "edad" : 2,
       "aptoDepto" : false
     },
     {
       "id" : 3,
-      "imagen" : "./assets/img/dog3.jpg",
+      "imagen" : "../assets/img/dog3.jpg",
       "nombre" : "Barba",
       "edad" : 2,
       "aptoDepto" : false
